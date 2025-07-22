eb3be7acc723124931f95beab3b2578db466191b
Mário Diego Rocha Valente <83090653+MarioDhiego@users.noreply.github.com>
2025-07-21 22:46:21 -0300

    Add files via upload

--- app.R
+++ app.R
@@ -28,24 +28,53 @@ ui <- fluidPage(
 
 server <- function(input, output, session){
   
-output$visualizar <- downloadHandler(
+
+    iframe <- eventReactive(input$visualizar, {
+      rmarkdown::render(
+        input = "relatorio.Rmd",
+        output_file = "www/relatorio.html",
+        params = list(pokemon = input$pokemon)
+      )
+      
+      tag$iframe(
+        src = "relatorio.html",
+        width = "100%",
+        heigth = 600
+      )
+      
+    })
+
+    output$preview <- renderUI({ 
+      req(iframe())
+      iframe()
+      
+    })
+    
+output$baixar <- downloadHandler(
   filename = function(){
   glue::glue("relatorio_{input$pokemon}.pdf")  
   },
   content =  function(file){
-   
-    rmarkdown::render(
-      input = "relatorio.Rmd",
-      output_file = "arquivo.html",
-      params = list(pokemon = input$pokemon)
-    )
     
-    pagedown::chrome_print(
-      input = "arquivo.html",
-      output = file
-    )
-    
-     
+    withProgress(message = "GERANDO O RELATÓRIO EM HTML...",  {
+      
+      incProgress(0.2)
+      arquivo_html <- tempfile(fileext = ".html")
+      
+      
+      rmarkdown::render(
+        input = "relatorio.Rmd",
+        output_file = "arquivo_html",
+        params = list(pokemon = input$pokemon)
+      )
+      
+      incProgress(0.4, message = "RELATÓRIO EM PDF...")
+      pagedown::chrome_print(
+        input = "arquivo.html",
+        output = file
+      )
+      
+    })
   }
   
   
